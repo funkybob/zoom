fca3de8aa898f04b6eb105702cf57539038665f6
Curtis Maloney <curtis@tinbrain.net>
2019-05-05 08:04:21 +1000

    Update signatures

--- comp.h
+++ comp.h
@@ -1,14 +1,15 @@
 #ifndef __COMP_H__
+#define __COMP_H__
 
 #include <stdint.h>
 #include <unistd.h>
 
 struct buffer {
-	size_t size;
+	uint32_t size;
 	uint8_t *data;
 };
 
-size_t compress(struct buffer *src, struct buffer *dest);
-size_t decompress(struct buffer *src, struct buffer *dest, size_t size);
+uint32_t compress(struct buffer *src, struct buffer *dest);
+uint32_t decompress(struct buffer *src, struct buffer *dest, uint32_t size);
 
 #endif
