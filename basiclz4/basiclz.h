6d4760ac358cfb4654bfcbdaf57d18c733d67b5a
Curtis Maloney <curtis@tinbrain.net>
2019-05-05 10:58:33 +1000

    Add LOG macro

--- basiclz4/basiclz.h
+++ basiclz4/basiclz.h
@@ -1,3 +1,10 @@
 #define MAX_FRAME_SIZE (1<<22)
 
 #define LAZY_PARSE 1
+
+#ifdef DEBUG
+#error DEBUGGERY!
+#define LOG(...)    { fprintf(__VA_ARGS__); fflush(stdout); }
+#else
+#define LOG(...)    /* __VA_ARGS__ */
+#endif
